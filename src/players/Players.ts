12932e858322ce28ad327f9d4ae8379b5f96a42c
nvaneethm <navaneethna.m@gmail.com>
2025-03-09 08:39:50 +0000

    Deploying to gh-pages from @ nvaneethm/nav-player@dc00f894eb067495c9ff0dc0f26b37b0ba8318a0 🚀

--- src/players/Players.ts
+++
@@ -1,7 +0,0 @@
-import { createPlayer, HLS_JS, SHAKA_PLAYER, VIDEO_JS } from './PlayerFactory';
-import PluginManager from './PluginManager';
-
-// Register plugins as factories
-PluginManager.registerPlugin(VIDEO_JS, () => createPlayer(VIDEO_JS));
-PluginManager.registerPlugin(SHAKA_PLAYER, () => createPlayer(SHAKA_PLAYER));
-PluginManager.registerPlugin(HLS_JS, () => createPlayer(HLS_JS));
